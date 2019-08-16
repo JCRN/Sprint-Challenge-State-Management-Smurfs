642fe574e9baa7184e2fc7493f06b803377cd7c9
JCRN <clemons.jason@gmail.com>
2019-08-16 08:50:28 -0700

    WIP: added/created/imported: contexts, axios GET, initial form component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default App;
+import './App.css'
+
+import { SmurfContext } from '../contexts/SmurfContext'
+import { SmurfsContext } from '../contexts/SmurfsContext'
+
+import { SmurfyForm } from './SmurfyForm'
+
+export default function App() {
+  const [smurfs, setSmurfs] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(results => {
+        console.log('get smurfs results: ', results)
+        setSmurfs(results.data)
+      })
+      .catch(error => console.log('get smurfs error: ', error))
+  }, [])
+
+  const postSmurf = smurf => {}
+
+  return (
+    <SmurfsContext.Provider value={{ smurfs, setSmurfs }}>
+      {/* <SmurfContext.Provider value={{ postSmurf }}> */}
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ CONTEXTS</h1>
+        <SmurfyForm />
+      </div>
+      {/* </SmurfContext.Provider> */}
+    </SmurfsContext.Provider>
+  )
+}
